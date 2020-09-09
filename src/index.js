b93bd6e6e338630ac530e8fd95d4954bb8c61fde
Даниил Яндыбаев <xramirezx@outlook.com>
2020-09-09 20:35:25 +0400

    feat: add solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,47 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  let opened_brackets_stack = [];
+
+  let config = {};
+
+  for (let brackets of bracketsConfig) {
+    if (brackets[0] !== brackets[1]) {
+      config[brackets[0]] = {
+        isOpenBracket: true,
+        isCloseBracket: false,
+        closingBracket: brackets[1],
+        openingBracket: null
+      }
+  
+      config[brackets[1]] = {
+        isOpenBracket: false,
+        isCloseBracket: true,
+        closingBracket: null,
+        openingBracket: brackets[0]
+      }
+    } else {
+      config[brackets[0]] = {
+        isOpenBracket: true,
+        isCloseBracket: true,
+        closingBracket: brackets[0],
+        openingBracket: brackets[0]
+      }
+    }
+  }
+
+  for (let bracket of str) {
+    if (config[bracket].isOpenBracket && config[bracket].isCloseBracket) {
+      if (opened_brackets_stack[opened_brackets_stack.length - 1] === bracket) {
+        opened_brackets_stack.pop();
+      } else {
+        opened_brackets_stack.push(bracket);
+      }
+    } else if (config[bracket].isOpenBracket) {
+      opened_brackets_stack.push(bracket);
+    } else if (config[bracket].isCloseBracket) {
+      if (opened_brackets_stack.length === 0) return false;
+      if (config[opened_brackets_stack.pop()].closingBracket !== bracket) return false;
+    }
+  }
+
+  return opened_brackets_stack.length === 0;
 }
